91d252ada4e723830988ed9ef6f7a10d56a2ffca
Garret Thompson <garret.thompson@gmail.com>
2022-04-18 13:55:04 -0400

    Add delay to parallel loading to demonstrate cancellation

--- src/main/kotlin/tasks/Request5Concurrent.kt
+++ src/main/kotlin/tasks/Request5Concurrent.kt
@@ -9,6 +9,7 @@ import contributors.logUsers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> =
     coroutineScope {
@@ -16,7 +17,7 @@ suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData)
       val contributors =
           repos.map { repo ->
             async {
-              log("starting loading for ${repo.name}")
+              log("starting loading for ${repo.name}").also { delay(3000) }
               service.getRepoContributors(req.org, repo.name).also { logUsers(repo, it) }.bodyList()
             }
           }
