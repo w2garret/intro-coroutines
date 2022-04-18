847dcd15827d06eb1e17e010f3445f5ccf38e301
Garret Thompson <garret.thompson@gmail.com>
2022-04-18 13:22:16 -0400

    Implement concurrent coroutines

--- src/main/kotlin/tasks/Request5Concurrent.kt
+++ src/main/kotlin/tasks/Request5Concurrent.kt
@@ -1,9 +1,22 @@
 package tasks
 
-import contributors.*
-import kotlinx.coroutines.*
+import contributors.GitHubService
+import contributors.RequestData
+import contributors.User
+import contributors.logRepos
+import contributors.logUsers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 
 suspend fun loadContributorsConcurrent(service: GitHubService, req: RequestData): List<User> =
     coroutineScope {
-  TODO()
-}
+      val repos = service.getOrgRepos(req.org).also { logRepos(req, it) }.bodyList()
+      val contributors =
+          repos.map { repo ->
+            async {
+              service.getRepoContributors(req.org, repo.name).also { logUsers(repo, it) }.bodyList()
+            }
+          }
+      contributors.awaitAll().flatten().aggregate()
+    }
