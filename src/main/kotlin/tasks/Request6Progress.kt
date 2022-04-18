5191f802a39b75e99cb56d2d103d9b31a7f8ea11
Garret Thompson <garret.thompson@gmail.com>
2022-04-18 14:23:50 -0400

    Implement loading with progress report but without parallel execution

--- src/main/kotlin/tasks/Request6Progress.kt
+++ src/main/kotlin/tasks/Request6Progress.kt
@@ -1,11 +1,25 @@
 package tasks
 
-import contributors.*
+import contributors.GitHubService
+import contributors.RequestData
+import contributors.User
+import contributors.logRepos
+import contributors.logUsers
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-  TODO()
+  val repos = service.getOrgRepos(req.org).also { logRepos(req, it) }.bodyList()
+  var contributors = listOf<User>()
+  for ((index, repo) in repos.withIndex()) {
+    val users =
+        service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+    contributors = (contributors + users).aggregate()
+    updateResults(contributors, index == repos.lastIndex)
+  }
 }
